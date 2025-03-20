720d070ebb60a09e7bd80b7a1f8fc1407500fa53
Debayan-Sarkar <debayansarkar368@gmail.com>
2025-03-20 13:09:33 +0530

    modified:   src/components/Body/Body.jsx 	modified:   src/components/Header/Header.jsx 	modified:   src/index.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import { MdMenu, MdMenuOpen } from 'react-icons/md'
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className=''>
+    <header className='w-full'>
       <nav className="bg-white/50  text-black p-4 shadow-lg">
-        <div className="container mx-auto flex justify-between items-center">
+        <div className="mx-auto flex justify-between items-center">
           <a href="#" className="text-2xl font-bold">Brand</a>
           <button
             className="md:hidden block text-black"
@@ -15,10 +15,10 @@ const Header = () => {
             {isOpen ? <MdMenuOpen size={28} /> : <MdMenu size={28} />}
           </button>
           <ul className={`md:flex md:space-x-6 md:items-center absolute md:static top-16 left-0 w-full bg-[#f0f8ff] md:w-auto md:bg-transparent md:flex-row transition-all duration-300 ease-in-out ${isOpen ? "h-[200px]" : "h-[0px]"}`}>
-            <li><a href="#" className={`block ${isOpen ? "" : "hidden"} transition-all duration-300 ease-in-out px-4 py-2 md:py-0 hover:text-gray-600`}>Home</a></li>
-            <li><a href="#" className={`block ${isOpen ? "" : "hidden"} transition-all duration-300 ease-in-out px-4 py-2 md:py-0 hover:text-gray-600`}>About</a></li>
-            <li><a href="#" className={`block ${isOpen ? "" : "hidden"} transition-all duration-300 ease-in-out px-4 py-2 md:py-0 hover:text-gray-600`}>Services</a></li>
-            <li><a href="#" className={`block ${isOpen ? "" : "hidden"} transition-all duration-300 ease-in-out px-4 py-2 md:py-0 hover:text-gray-600`}>Contact</a></li>
+            <li><a href="#" className={`block ${isOpen ? "" : "max-md:hidden"} transition-all duration-300 ease-in-out px-4 py-2 md:py-0 hover:text-gray-600`}>Home</a></li>
+            <li><a href="#" className={`block ${isOpen ? "" : "max-md:hidden"} transition-all duration-300 ease-in-out px-4 py-2 md:py-0 hover:text-gray-600`}>About</a></li>
+            <li><a href="#" className={`block ${isOpen ? "" : "max-md:hidden"} transition-all duration-300 ease-in-out px-4 py-2 md:py-0 hover:text-gray-600`}>Services</a></li>
+            <li><a href="#" className={`block ${isOpen ? "" : "max-md:hidden"} transition-all duration-300 ease-in-out px-4 py-2 md:py-0 hover:text-gray-600`}>Contact</a></li>
           </ul>
         </div>
       </nav>
